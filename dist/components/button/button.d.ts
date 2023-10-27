3be62c8717b0f40ad2e3a5eb9cb7c3e5623c8526
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-27 17:07:00 -0300

    hfghfghfg

---
+++ dist/components/button/button.d.ts
@@ -0,0 +1,4 @@
+import { ButtonProps } from "./button.model";
+import React from "react";
+import "./button.css";
+export declare const Button: (props: ButtonProps) => React.JSX.Element;
---
+++ dist/components/button/button.model.d.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+export declare type ButtonProps = {
+    density?: "medium";
+    style?: "brand" | "danger";
+    type: "button";
+    emphasis: "primary";
+    progress?: boolean;
+    children?: ReactNode;
+};
